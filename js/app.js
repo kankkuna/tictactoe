b7b85e5cbd6bc897c9a07e3bfcf390c66e5ef44a
kankkuna <karina842@hotmail.com>
2017-11-09 12:51:19 -0500

    subiendo html css js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  var board = document.querySelector('.board-js');
+
+  board.addEventListener('click', addX);
+};
+var centinel = false;
+
+function addX(event) {
+  if (centinel)
+    event.target.textContent = 'X';
+  else
+    event.target.textContent = 'O';
+  centinel = !centinel;
+}
